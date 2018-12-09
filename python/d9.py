54dc70373df5c1421ef2d7330ef5964b36680dfb
James <james.ducker@versent.com.au>
2018-12-09 17:40:45 +1100

    Code clean-up

--- python/d9.py
+++ python/d9.py
@@ -9,17 +9,13 @@ last = 7162600
 scores = [0] * players
 # Init the marble ring
 ring = collections.deque([0])
-currentMarble = 0
 
 for turn in range(1, last + 1):
-	player = (turn - 1) % players
-
-	# Marble is a multiple of 23
+	# If marble is a multiple of 23
 	if not turn % 23:
 		ring.rotate(-7)
-		scores[player] += turn + ring.pop()
+		scores[(turn - 1) % players] += turn + ring.pop()
 	else:
-		insertIndex = (currentMarble + 2 - 1) % (len(ring)) + 1
 		ring.rotate(2)
 		ring.append(turn)
 
